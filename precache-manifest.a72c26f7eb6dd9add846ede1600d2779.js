4bed3ad7294c86cbbf393388ec464f084e4889c8
Alex K.D <md.alexkumar@gmail.com>
2023-01-10 15:53:32 +0530

    Updates

---
+++ precache-manifest.a72c26f7eb6dd9add846ede1600d2779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa15c83da80f962f0712690c8bc94df1",
+    "url": "/md-alexkumar/solar-square-hooks.git/index.html"
+  },
+  {
+    "revision": "67bfbf1c462a66f9f4bb",
+    "url": "/md-alexkumar/solar-square-hooks.git/static/css/main.58117526.chunk.css"
+  },
+  {
+    "revision": "dc93222c34f6af62cec5",
+    "url": "/md-alexkumar/solar-square-hooks.git/static/js/2.a0ab9a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/md-alexkumar/solar-square-hooks.git/static/js/2.a0ab9a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bfbf1c462a66f9f4bb",
+    "url": "/md-alexkumar/solar-square-hooks.git/static/js/main.456026d1.chunk.js"
+  },
+  {
+    "revision": "27b73620f86fdc340df9",
+    "url": "/md-alexkumar/solar-square-hooks.git/static/js/runtime-main.789feece.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/md-alexkumar/solar-square-hooks.git/precache-manifest.a72c26f7eb6dd9add846ede1600d2779.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/md-alexkumar/solar-square-hooks.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
