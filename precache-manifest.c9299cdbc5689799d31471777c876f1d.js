77de31f5b8dd925ff8ca557c4c63560db3562774
Alex K.D <md.alexkumar@gmail.com>
2023-01-10 15:58:00 +0530

    Updates

---
+++ precache-manifest.c9299cdbc5689799d31471777c876f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94be59cd9101be6e8d61f09c00f71dd",
+    "url": "/solar-square-hooks/index.html"
+  },
+  {
+    "revision": "59cb3a80bd56b6857292",
+    "url": "/solar-square-hooks/static/css/main.58117526.chunk.css"
+  },
+  {
+    "revision": "dc93222c34f6af62cec5",
+    "url": "/solar-square-hooks/static/js/2.a0ab9a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/solar-square-hooks/static/js/2.a0ab9a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59cb3a80bd56b6857292",
+    "url": "/solar-square-hooks/static/js/main.4f8f7aa9.chunk.js"
+  },
+  {
+    "revision": "7cc16e42214476f39c16",
+    "url": "/solar-square-hooks/static/js/runtime-main.b198bac1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/md-alexkumar/solar-square-hooks.git/precache-manifest.a72c26f7eb6dd9add846ede1600d2779.js"
+  "/solar-square-hooks/precache-manifest.c9299cdbc5689799d31471777c876f1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/md-alexkumar/solar-square-hooks.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solar-square-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
